fce14ac9998eb5619f2de0f06ecbdd49b764f178
Arne Fostvedt <arne@Arnes-MacBook-Air.local>
2012-12-12 12:08:35 -0800

    updated parser module with net/http connection boilerplate

--- lib/web_parser.rb
+++ lib/web_parser.rb
@@ -1,3 +1,24 @@
 module WebHandler
 
+  def self.data_from_url(url)
+    query_api(url)
+  end
+
+  def self.set_connection_parameters(url, port = 80)
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, port)
+    if port == 443
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    end
+    request = Net::HTTP::Get.new(uri.request_uri, initheader = {"Authorization" => "token #{OAUTH_TOKEN}"})
+    [request, http]
+  end
+
+  def self.query_api(url)
+    request, http = self.set_connection_parameters(url, 443)
+    response = http.request(request)
+    json_body = JSON.parse(response.body)
+  end
+
 end
