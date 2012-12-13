5a37f4ddc694d4efca01cc575bcee30b3a1dfa2f
Arne Fostvedt <arne@Arnes-MacBook-Air.local>
2012-12-12 17:57:24 -0800

    added document spec outline

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -3,9 +3,19 @@ require 'spec_helper'
 describe Document do 
   context 'document provided via URL' do
 
+    it 'returns an error when an invalid URL is passed'
+
+    it 'fires off an HTTP request when a valid URL is passed'
+
+    it 'pulls in the body of an HTTP request'
+
+    it 'removes the HTML content if applicable'
+
+    it 'stores the HTTP body in the database BODY column'
+
   end
 
   context 'document provided via upload' do
-    
   end
+
 end
