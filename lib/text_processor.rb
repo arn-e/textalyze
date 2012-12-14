9ddbe1b5dc4d66e54505c30031078b31738bc5de
Arne Fostvedt <arne@Arnes-MacBook-Air.local>
2012-12-14 00:33:20 -0800

    updated README

--- lib/text_processor.rb
+++ lib/text_processor.rb
@@ -7,8 +7,6 @@ module TextProcessor
   def self.word_frequency(body, container = [], result_set = [])
     words = TfIdf.new(container.push(body.split)).tf
     { :word_frequency => words }
-    # words.first.each {|i| result_set << i[0]}
-    # result_set[0..5]
   end
 
 end
