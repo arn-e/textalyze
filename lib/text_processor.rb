8ff88e8db9bf6a52780fbd2199fe4d17505c7f01
Arne Fostvedt <arne@Arnes-MacBook-Air.local>
2012-12-29 19:29:49 -0500

    updated text processor to sort word frequency output

--- lib/text_processor.rb
+++ lib/text_processor.rb
@@ -5,8 +5,10 @@ module TextProcessor
   end
 
   def self.word_frequency(body, container = [], result_set = [])
+    sorted = {}
     words = TfIdf.new(container.push(body.split)).tf
-    { :word_frequency => words[0] }
+    words[0].sort_by {|key, value| value}.reverse.each {|elem| sorted[elem[0]] = elem[1]}
+    { :word_frequency => sorted }
   end
 
   def self.rid_analysis(body, category_number = 1)
